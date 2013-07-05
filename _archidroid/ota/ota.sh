e41de2c0d6416a403bfe80419a6b4bb4c4ad9e90
JustArchi <archipl33@gmail.com>
2013-07-05 17:29:43 +0200

    otafix2

--- _archidroid/ota/ota.sh
+++ _archidroid/ota/ota.sh
@@ -5,4 +5,11 @@ echo "# ArchiDroid OTA" >> /system/build.prop
 echo "# Please don't remove these lines, they're needed for OTA" >> /system/build.prop
 echo "updateme.name=ArchiDroid2" >> /system/build.prop
 echo "updateme.version=2.0.0" >> /system/build.prop
+echo "updateme.otauid=archidroid2" >> /system/build.prop
+echo "updateme.urlcheck=https://dl.dropboxusercontent.com/u/23869279/ArchiDroid2/OTA/update_me_check.xml" >> /system/build.prop
+echo "updateme.urlelement=https://dl.dropboxusercontent.com/u/23869279/ArchiDroid2/OTA/update_me_parts.xml" >> /system/build.prop
+echo "updateme.reboottype=1" >> /system/build.prop
+echo "updateme.downloaddir=/mnt/sdcard/downloadDir" >> /system/build.prop
+echo "updateme.disableinstalledapps=1" >> /system/build.prop
+echo "updateme.disablescripts=1" >> /system/build.prop
 echo "# Thank you!" >> /system/build.prop

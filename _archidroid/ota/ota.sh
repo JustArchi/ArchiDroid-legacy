3355220d4d8a4be7c48e3763cde118d750143e89
JustArchi <archipl33@gmail.com>
2013-07-05 17:26:03 +0200

    otafix

--- _archidroid/ota/ota.sh
+++ _archidroid/ota/ota.sh
@@ -4,4 +4,5 @@
 echo "# ArchiDroid OTA" >> /system/build.prop
 echo "# Please don't remove these lines, they're needed for OTA" >> /system/build.prop
 echo "updateme.name=ArchiDroid2" >> /system/build.prop
-echo "updateme.version=1.9.9" >> /system/build.prop
+echo "updateme.version=2.0.0" >> /system/build.prop
+echo "# Thank you!" >> /system/build.prop

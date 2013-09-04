03daf2d97e040ec09e192f4b9689a28bb4bdc3e4
JustArchi <archipl33@gmail.com>
2013-09-04 14:04:19 +0200

    AD_TempRoot Bugfix #1

--- _archidroid/auto/data/media/0/ArchiDroid/Scripts/ArchiDroid_TemporaryUnroot.sh
+++ _archidroid/auto/data/media/0/ArchiDroid/Scripts/ArchiDroid_TemporaryUnroot.sh
@@ -1,7 +1,7 @@
 #!/system/bin/sh
 
 # Check if we're not being executed twice
-if [ -e /system/etc/init.d/97ArchiDroid_RestoreRoot ]; then
+if [ -e /system/etc/init.d/91ArchiDroid_RestoreRoot ]; then
 	echo "Sorry but it looks like you're already unrooted till next reboot"
 	exit 1
 fi
@@ -13,14 +13,16 @@ if [ `id -u | grep 'root' | wc -l` -lt 1 ]; then
 fi
 
 sysrw
+mv /system/bin/su /system/bin/ArchiDroid_r00t
 mv /system/xbin/su /system/xbin/ArchiDroid_r00t
 echo "#!/system/bin/sh
 sysrw
-mv /system/xbin/ArchiDroid_r00t /system/xbin/su
-rm -f $0
-sysro
-exit 0" > /system/etc/init.d/97ArchiDroid_RestoreRoot
-chmod 755 /system/etc/init.d/97ArchiDroid_RestoreRoot
+mv /system/bin/ArchiDroid_r00t /system/bin/su
+mv /system/xbin/ArchiDroid_r00t /system/xbin/su" > /system/etc/init.d/91ArchiDroid_RestoreRoot
+echo 'rm -f $0' >> /system/etc/init.d/91ArchiDroid_RestoreRoot
+echo "sysro
+exit 0" > /system/etc/init.d/91ArchiDroid_RestoreRoot
+chmod 755 /system/etc/init.d/91ArchiDroid_RestoreRoot
 sysro
 echo "Done, ArchiDroid is now fully unrooted till next reboot"
 exit 0

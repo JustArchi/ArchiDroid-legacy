308958cc26d8f074514ca52bef60285697a86fbf
JustArchi <archipl33@gmail.com>
2013-09-07 22:59:10 +0200

    Bump to 2.1.4 + misc

--- __dont_include/build.sh
+++ __dont_include/build.sh
@@ -4,8 +4,8 @@
 # Not Disabled
 #exit 1
 
-WERSJA="ro.build.display.id=ArchiDroid 2.1.3"
-OTA="echo \"updateme.version=2.1.3\" >> /system/build.prop"
+WERSJA="ro.build.display.id=ArchiDroid 2.1.4"
+OTA="echo \"updateme.version=2.1.4\" >> /system/build.prop"
 DENSITY="#ro.sf.lcd_density=320"
 
 function zamien {

f683f275997f055732fcc71e9f3db88279a49c7a
JustArchi <JustArchi@JustArchi.net>
2013-09-12 02:36:49 +0200

    misc

--- __dont_include/openpdroid.sh
+++ __dont_include/openpdroid.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 cd ..
 
+# Disabled for a while
+rm -f cm-*.zip
+exit 0
+
 # Let's make it more universal (also benefit temasek's users) because I'm not using updater-script anyway
 # Cleanup, updates and patching
 cd /root/git/auto-patcher

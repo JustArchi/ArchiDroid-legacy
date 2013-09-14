c8de461588ec1351e9081d6ca00b1e0fd5d91f8b
JustArchi <JustArchi@JustArchi.net>
2013-09-14 22:48:11 +0200

    More improvements

---
+++ _archidroid/auto/data/media/0/ArchiDroid/Scripts/ArchiDroid_RestoreEFS.sh
@@ -0,0 +1,21 @@
+#!/system/bin/sh
+
+# Check if we're running as root
+if [ `id -u | grep 'root' | wc -l` -lt 1 ]; then
+	echo "Sorry but you need to execute this script as root"
+	exit 1
+fi
+
+if [ -z `which busybox` ]; then
+	echo "Sorry but it looks like you don't have required components. Are you using ArchiDroid?"
+	exit 1
+fi
+
+if [ ! -e /data/media/0/ArchiDroid/Backups/efs.tar.gz ]; then
+	echo "Sorry but it looks like you don't have any ArchiDroid backup available. What are you trying to achieve?"
+	exit 1
+fi
+
+busybox tar -zxvf /data/media/0/ArchiDroid/Backups/efs.tar.gz -C /
+reboot
+exit 0

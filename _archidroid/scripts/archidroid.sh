681d64f83d869927bb2541cb6d90195a777cee2e
JustArchi <JustArchi@JustArchi.net>
2013-09-16 09:37:19 +0200

    Merge media2zero into archidroid scripts

--- _archidroid/scripts/archidroid.sh
+++ _archidroid/scripts/archidroid.sh
@@ -1,5 +1,22 @@
 #!/tmp/bash
 
+if [ ! -d /data/media/0 ]; then
+	if [ -f /data/media/0 ]; then
+		rm -f /data/media/0
+	fi
+	cd /data/media
+	FILES=`ls`
+	mkdir -p /data/media/0
+	
+	# Just to be sure
+	mkdir -p /data/media/obb
+	mkdir -p /data/media/legacy
+	
+	for i in $FILES; do
+		mv "$i" 0/
+	done
+fi
+
 if [ ! -d /data/media/0/ArchiDroid ]; then
 	mkdir -p /data/media/0/ArchiDroid
 fi

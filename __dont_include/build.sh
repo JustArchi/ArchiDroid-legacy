48e5a6471b159c4eed6afcec9a349c7a8c6520b3
JustArchi <JustArchi@JustArchi.net>
2013-10-12 20:44:07 +0200

    misc

--- __dont_include/build.sh
+++ __dont_include/build.sh
@@ -9,7 +9,7 @@ SOURCE=1
 
 VERSION="2.2 EXPERIMENTAL"
 WERSJA="ro.build.display.id=ArchiDroid $VERSION"
-OTA="echo \"updateme.version=2.2\" >> /system/build.prop"
+OTA="echo \"updateme.version=2.2.0\" >> /system/build.prop"
 DENSITY="#ro.sf.lcd_density=320"
 
 function zamien {

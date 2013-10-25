1a084ece4009431640d13ff58b7e0d4040a9a929
JustArchi <JustArchi@JustArchi.net>
2013-10-26 00:08:01 +0200

    dont include update

--- __dont_include/updaterepos.sh
+++ __dont_include/updaterepos.sh
@@ -4,25 +4,34 @@ INIT=0
 
 contains () {
 	local e
-	for e in "${@:2}"; do 
+	for e in "${@:2}"; do
 		[[ "$e" == "$1" ]] && return 0
 	done
 	return 1
 }
 
+addUpstream () {
+	#ourBranch=`git rev-parse --abbrev-ref HEAD`
+	ourName=`basename "$folder"`
+	git remote remove $CMRepo > /dev/null 2>&1
+	git remote remove $crDroidRepo > /dev/null 2>&1
+	git remote add cm $CMRepoLink/$ourName.git
+	git remote add upstream $crDroidRepoLink/$ourName.git
+}
+
 # Packages in great sync with CM, we can literally pull them from CM repo to merge changes
-inSyncWithCM=("android_vendor_cm" "android_frameworks_native" "android_packages_apps_Nfc" "android_packages_providers_MediaProvider" "android_packages_inputmethods_LatinIME" "android_packages_apps_Phone" "android_packages_apps_Mms" "android_packages_apps_Gallery2" "android_packages_apps_Email" "android_packages_apps_Dialer" "android_packages_apps_Contacts" "android_packages_apps_ContactsCommon" "android_packages_apps_Calculator")
+inSyncWithCM=("android_frameworks_base" "android_packages_apps_Settings" "android_vendor_cm" "android_frameworks_native" "android_packages_apps_Nfc" "android_packages_providers_MediaProvider" "android_packages_inputmethods_LatinIME" "android_packages_apps_Phone" "android_packages_apps_Mms" "android_packages_apps_Gallery2" "android_packages_apps_Email" "android_packages_apps_Dialer" "android_packages_apps_Contacts" "android_packages_apps_ContactsCommon" "android_packages_apps_Calculator")
 
 # Packages NOT in great sync with CM, we should wait for Cristiano's merges if possible
-NOTinSyncWithCM=("android_frameworks_base" "android_packages_apps_Settings")
+NOTinSyncWithCM=("")
 
 # Branches
 crDroid="cr-main-10.2"
-CM="cm-10.2"
-
-# Local names
 crDroidRepo="upstream"
+crDroidRepoLink="https://github.com/cristianomatos"
+CM="cm-10.2"
 CMRepo="cm"
+CMRepoLink="https://github.com/CyanogenMod"
 ourRepo="origin"
 
 for folder in `find . -mindepth 1 -maxdepth 1 -type d` ; do
@@ -30,10 +39,7 @@ for folder in `find . -mindepth 1 -maxdepth 1 -type d` ; do
 	ourBranch=`git rev-parse --abbrev-ref HEAD`
 	ourName=`basename "$folder"`
 	if [ $INIT -eq 1 ]; then
-		git remote remove cm
-		git remote remove upstream
-		git remote add cm https://github.com/CyanogenMod/$ourName.git
-		git remote add upstream https://github.com/cristianomatos/$ourName.git
+		addUpstream
 	else
 		git pull $crDroidRepo $crDroid
 		if [ $? -ne 0 ]; then

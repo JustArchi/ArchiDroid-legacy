d4ec003d95a9598ea1615660741d7a8be5aff1d7
JustArchi <JustArchi@JustArchi.net>
2013-10-27 03:15:46 +0100

    dont include update

--- __dont_include/updaterepos.sh
+++ __dont_include/updaterepos.sh
@@ -19,10 +19,13 @@ addUpstream () {
 	git remote add upstream $crDroidRepoLink/$ourName.git
 }
 
-# Packages in great sync with CM, we can literally pull them from CM repo to merge changes
-inSyncWithCM=("android_frameworks_base" "android_packages_apps_Settings" "android_vendor_cm" "android_frameworks_native" "android_packages_apps_Nfc" "android_packages_providers_MediaProvider" "android_packages_inputmethods_LatinIME" "android_packages_apps_Phone" "android_packages_apps_Mms" "android_packages_apps_Gallery2" "android_packages_apps_Email" "android_packages_apps_Dialer" "android_packages_apps_Contacts" "android_packages_apps_ContactsCommon" "android_packages_apps_Calculator")
+# Packages in perfect sync with CM. We should pull them directly from CyanogenMod and merge any conflicts (if any)
+inPerfectSyncWithCM=("android_vendor_cm" "android_frameworks_native" "android_packages_apps_Nfc" "android_packages_providers_MediaProvider" "android_packages_inputmethods_LatinIME" "android_packages_apps_Phone" "android_packages_apps_Mms" "android_packages_apps_Gallery2" "android_packages_apps_Email" "android_packages_apps_Dialer" "android_packages_apps_Contacts" "android_packages_apps_ContactsCommon" "android_packages_apps_Calculator")
 
-# Packages NOT in great sync with CM, we should wait for Cristiano's merges if possible
+# Packages not in perfect sync with CM but still good enough. For these we should sync automatically only non-conflicting commits and eventually cherry-pick the rest, as we should stay in sync with the upstream.
+inSyncWithCM=("android_frameworks_base" "android_packages_apps_Settings")
+
+# Packages NOT in great sync with CM, we should wait for upstream merges
 NOTinSyncWithCM=("")
 
 # Branches
@@ -45,11 +48,13 @@ for folder in `find . -mindepth 1 -maxdepth 1 -type d` ; do
 		if [ $? -ne 0 ]; then
 			read -p "Something went wrong, please check and tell me when you're done, master!" -n1 -s
 		fi
-		if `contains "$ourName" "${inSyncWithCM[@]}"`; then
+		if `contains "$ourName" "${inPerfectSyncWithCM[@]}"`; then
+			git pull $CMRepo $CM
+		elif `contains "$ourName" "${inSyncWithCM[@]}"`; then
 			git pull -s ours $CMRepo $CM
-			if [ $? -ne 0 ]; then
-				read -p "Something went wrong, please check and tell me when you're done, master!" -n1 -s
-			fi
+		fi
+		if [ $? -ne 0 ]; then
+			read -p "Something went wrong, please check and tell me when you're done, master!" -n1 -s
 		fi
 		git push $ourRepo $ourBranch
 		if [ $? -ne 0 ]; then

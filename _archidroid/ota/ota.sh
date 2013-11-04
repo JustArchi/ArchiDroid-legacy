68c286adcb1c150f4b05f44d0e9667576e4c968b
JustArchi <JustArchi@JustArchi.net>
2013-11-04 16:00:16 +0100

    misc

--- _archidroid/ota/ota.sh
+++ _archidroid/ota/ota.sh
@@ -4,8 +4,8 @@
 echo "# ArchiDroid OTA" >> /system/build.prop
 echo "# Please don't remove these lines, they're needed for OTA" >> /system/build.prop
 echo "updateme.name=ArchiDroid" >> /system/build.prop
-echo "updateme.version=1.7" >> /system/build.prop
-echo "updateme.otauid=archidroid2" >> /system/build.prop
+echo "updateme.version=1.7.0" >> /system/build.prop
+echo "updateme.otauid=archidroid" >> /system/build.prop
 echo "updateme.urlcheck=https://dl.dropboxusercontent.com/u/23869279/ArchiDroid/OTA/update_me_check.xml" >> /system/build.prop
 echo "updateme.urlelement=https://dl.dropboxusercontent.com/u/23869279/ArchiDroid/OTA/update_me_parts.xml" >> /system/build.prop
 echo "updateme.reboottype=1" >> /system/build.prop

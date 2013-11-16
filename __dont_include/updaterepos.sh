7b63225733a08fce6ade32fa182c50b1adcf812d
JustArchi <JustArchi@JustArchi.net>
2013-11-16 22:05:56 +0100

    Update updaterepos.sh

--- __dont_include/updaterepos.sh
+++ __dont_include/updaterepos.sh
@@ -30,7 +30,8 @@ inSyncWithCM=("android_frameworks_base" "android_packages_apps_Settings")
 droppedFromUpstream=("android_vendor_cm")
 
 # Branches
-crDroid="cm-4.4"
+crDroid="cr-4.4"
+crDroidExtra="cm-4.4"
 crDroidRepo="upstream"
 crDroidRepoLink="https://github.com/cristianomatos"
 CM="cm-11.0"
@@ -48,6 +49,11 @@ for folder in `find . -mindepth 1 -maxdepth 1 -type d` ; do
 		git pull $ourRepo $ourBranch
 		if ! `contains "$ourName" "${droppedFromUpstream[@]}"`; then
 			git pull $crDroidRepo $crDroid
+			if [ $? -ne 0 ]; then
+				# This is mandatory, we MUST stay in sync with upstream
+				read -p "Something went wrong, please check and tell me when you're done, master!" -n1 -s
+			fi
+			git pull $crDroidRepo $crDroidExtra
 			if [ $? -ne 0 ]; then
 				# This is mandatory, we MUST stay in sync with upstream
 				read -p "Something went wrong, please check and tell me when you're done, master!" -n1 -s

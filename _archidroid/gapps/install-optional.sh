9c5ac51ec871985e7a1eb560e5a814a788772467
JustArchi <JustArchi@JustArchi.net>
2013-11-16 15:45:23 +0100

    PA GAPPS 4.4

--- _archidroid/gapps/install-optional.sh
+++ _archidroid/gapps/install-optional.sh
@@ -18,12 +18,15 @@ good_ffc_device() {
 
 if good_ffc_device && [ -e /system/etc/permissions/android.hardware.camera.front.xml ]; then
   echo "Installing face detection support"
+  rm  -f /system/addon.d/71-gapps-faceunlock.sh
   cp -a /tmp/face/* /system/
-  chmod 755 /system/addon.d/71-gapps-faceunlock.sh
+  chmod 755 /system/addon.d/71-faceunlock.sh
 elif  [ -d /system/vendor/pittpatt/ ]; then
   rm -rf /system/vendor/pittpatt/
   rm  -f /system/app/FaceLock.apk
+  rm  -f /system/app/FaceLock.odex
   rm  -f /system/lib/libfacelock_jni.so
   rm  -f /system/addon.d/71-gapps-faceunlock.sh
+  rm  -f /system/addon.d/71-faceunlock.sh
 fi
 rm -rf /tmp/face

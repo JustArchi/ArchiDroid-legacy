b35135ae149cd3786a19ea76599ba2071f589d2a
JustArchi <JustArchi@JustArchi.net>
2013-12-07 21:51:26 +0100

    misc

--- _archidroid/auto/data/media/0/ArchiDroid/Scripts/ArchiDroid_RemoveArchiDroid.sh
+++ _archidroid/auto/data/media/0/ArchiDroid/Scripts/ArchiDroid_RemoveArchiDroid.sh
@@ -6,17 +6,7 @@ if [ `whoami` != "root" ]; then
 	exit 1
 fi
 
-if [ -z `which sysrw` ] || [ -z `which sysro` ]; then
-	echo "Sorry but it looks like you don't have required components. Are you using ArchiDroid?"
-	exit 1
-fi
-
-sysrw
-cd /system/etc/init.d
-for FILE in `find . -name "*ArchiDroid*"` ; do
-	rm -f $FILE
-done
-sysro
 rm -rf /data/media/0/ArchiDroid
-reboot recovery
+#reboot recovery
+echo "Done, ArchiDroid folder removed"
 exit 0

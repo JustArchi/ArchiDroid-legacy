8abf5d8ce44e259f2d206104d88b1eb1fd32d8a1
JustArchi <JustArchi@JustArchi.net>
2013-12-11 21:01:03 +0100

    EXPERIMENTAL: First adflash git version

---
+++ _archidroid/auto/data/media/0/ArchiDroid/GitHub/github.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+AD="/data/media/0/ArchiDroid"
+ourFolder="$AD/GitHub"
+ourRepo="origin"
+ourLink="git://github.com/JustArchi/ArchiDroid.git"
+VERSION="$1"
+
+if [ -z "$VERSION" ]; then
+	exit 1
+fi
+
+if [ -d $ourFolder/$VERSION ]; then
+	echo "Ok, repository already available with branch $VERSION. Updating..."
+	cd $ourFolder/$VERSION
+	git pull $ourRepo $VERSION
+else
+	echo "Repository isn't available yet, cloning ArchiDroid repository branch $VERSION"
+	git clone --branch $VERSION --depth 1 $ourLink $VERSION
+	cd $ourFolder/$VERSION
+	git pull $ourRepo $VERSION
+fi
+
+exit 0

2b716d2e9f953885e194fb2a75957323922ef5ad
JustArchi <JustArchi@JustArchi.net>
2013-12-13 06:38:53 +0100

    adflash: A few enhancements

--- _archidroid/auto/data/media/0/ArchiDroid/GitHub/github.sh
+++ _archidroid/auto/data/media/0/ArchiDroid/GitHub/github.sh
@@ -9,10 +9,22 @@ if [ -z "$VERSION" ]; then
 	exit 1
 fi
 
+echo "CHANGELOG START"
+echo ""
+curl -kSs https://raw.github.com/JustArchi/ArchiDroid/$VERSION/META-INF/com/google/android/aroma/_changelog.txt
+echo ""
+echo ""
+echo "CHANGELOG EOF"
+
 if [ -d $ourFolder/$VERSION ]; then
 	echo "Ok, repository already available with branch $VERSION. Updating..."
 	cd $ourFolder/$VERSION
+	ourOldHead=`git rev-parse HEAD`
 	git pull $ourRepo $VERSION
+	echo "New commits:"
+	git --no-pager log --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%Creset commited by %C(bold blue)%an%Creset' $ourOldHead..$ourRepo/$VERSION
+	echo ""
+	echo ""
 else
 	echo "Repository isn't available yet, cloning ArchiDroid repository branch $VERSION"
 	cd $ourFolder

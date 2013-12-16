b2a15b0584a894d42f95b33572d71f02604f577c
JustArchi <JustArchi@JustArchi.net>
2013-12-16 15:31:44 +0100

    Add support for even more weird whoami output

--- _archidroid/auto/data/media/0/ArchiDroid/Scripts/ArchiDroid_FstrimAll.sh
+++ _archidroid/auto/data/media/0/ArchiDroid/Scripts/ArchiDroid_FstrimAll.sh
@@ -1,7 +1,7 @@
 #!/system/bin/sh
 
 # Check if we're running as root
-if [ -z `whoami | grep -i "root"` ] && [ -z `whoami | grep -i "uid 0"` ]; then
+if [ `whoami 2>&1 | grep -i "root" | wc -l` -eq 0 ] && [ `whoami 2>&1 | grep -i "uid 0" | wc -l` -eq 0 ]; then
 	echo "Sorry but you need to execute this script as root"
 	exit 1
 fi

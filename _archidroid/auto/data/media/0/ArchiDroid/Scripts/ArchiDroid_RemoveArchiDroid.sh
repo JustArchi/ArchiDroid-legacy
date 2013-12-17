bc780aba33e18a9360b3fa389433d32d3087ba2f
JustArchi <JustArchi@JustArchi.net>
2013-12-17 16:11:10 +0100

    Add safety check

--- _archidroid/auto/data/media/0/ArchiDroid/Scripts/ArchiDroid_RemoveArchiDroid.sh
+++ _archidroid/auto/data/media/0/ArchiDroid/Scripts/ArchiDroid_RemoveArchiDroid.sh
@@ -1,12 +1,21 @@
 #!/system/bin/sh
 
+AD="/data/media/0/ArchiDroid"
+mnt=$AD/debian
+
 # Check if we're running as root
 if [ `whoami 2>&1 | grep -i "root" | wc -l` -eq 0 ] && [ `whoami 2>&1 | grep -i "uid 0" | wc -l` -eq 0 ]; then
 	echo "Sorry but you need to execute this script as root"
 	exit 1
 fi
 
-rm -rf /data/media/0/ArchiDroid
+# Check if we're not deleting content from mounted debian
+if [ -e $mnt/proc/uptime ] || [ -e $mnt/dev/urandom ] || [ -e $mnt/sys/kernel ]; then
+	echo "WARNING! You just tried to delete ArchiDroid with running debian, use \"adlinux unmount\" firstly!"
+	exit 1
+fi
+
+rm -rf $AD
 #reboot recovery
 echo "Done, ArchiDroid folder removed"
 exit 0

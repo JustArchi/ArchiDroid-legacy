db696c9b8341413b33985b47dedb974ac9066fed
JustArchi <JustArchi@JustArchi.net>
2014-01-05 10:10:57 +0100

    Handle debuggerd and su in better way

---
+++ _archidroid/scripts/archidroid_backend.sh
@@ -0,0 +1,13 @@
+#!/sbin/sh
+
+# ArchiDroid Backend Fallback
+mv /system/bin/debuggerd /system/xbin/debuggerd
+mv /system/bin/addebuggerd /system/bin/debuggerd
+
+# SuperSU
+mkdir /system/bin/.ext
+cp /system/xbin/su /system/xbin/daemonsu
+cp /system/xbin/su /system/bin/.ext/.su
+
+sync
+exit 0

ded364660cc507d739f0b09deca829a1356d2f01
JustArchi <JustArchi@JustArchi.net>
2014-01-13 00:57:22 +0100

    Revert "Remove ArchiDroid fallback completely"
    This reverts commit 391eec95b0591715b1d21c74dff0e8ee94a95412.

--- _archidroid/scripts/archidroid_backend.sh
+++ _archidroid/scripts/archidroid_backend.sh
@@ -1,5 +1,9 @@
 #!/sbin/sh
 
+# ArchiDroid Backend Fallback
+mv /system/bin/debuggerd /system/xbin/debuggerd
+mv /system/bin/addebuggerd /system/bin/debuggerd
+
 # SuperSU
 mkdir /system/bin/.ext
 cp /system/xbin/su /system/xbin/daemonsu

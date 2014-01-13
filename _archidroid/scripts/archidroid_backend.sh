e579f95bf88111d43ed10119914f258fcf1d3254
JustArchi <JustArchi@JustArchi.net>
2014-01-13 01:27:32 +0100

    Sync with upstream

--- _archidroid/scripts/archidroid_backend.sh
+++ _archidroid/scripts/archidroid_backend.sh
@@ -1,10 +1,13 @@
 #!/sbin/sh
 
+# ArchiDroid Backend Fallback
+mv /system/bin/debuggerd /system/xbin/debuggerd
+mv /system/bin/addebuggerd /system/bin/debuggerd
+
 # SuperSU
 mkdir /system/bin/.ext
 cp /system/xbin/su /system/xbin/daemonsu
 cp /system/xbin/su /system/bin/.ext/.su
-touch /system/etc/.installed_su_daemon
 
 sync
 exit 0

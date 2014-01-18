d681bc00a055d63171dd3a204fc632d6a9f18183
JustArchi <JustArchi@JustArchi.net>
2014-01-18 20:19:32 +0100

    ADBLOCK: Add an option to switch between hosts

---
+++ _archidroid/auto/system/archidroid/scripts/ArchiDroid_Adblock_MoabAdawayHostsSwitch.sh
@@ -0,0 +1,32 @@
+#!/system/bin/sh
+# ArchiDroid Script
+
+# Check if we're running as root
+if [ `whoami 2>&1 | grep -i "root" | wc -l` -eq 0 ] && [ `whoami 2>&1 | grep -i "uid 0" | wc -l` -eq 0 ]; then
+	echo "Sorry but you need to execute this script as root"
+	exit 1
+fi
+
+ADDEV="/system/archidroid/dev"
+sysrw
+
+if [ -e $ADDEV/ADBLOCK_USE_ADAWAY_HOSTS ]; then
+	rm -f /system/archidroid/etc/hosts
+	ln -s /system/archidroid/etc/hosts_moab /system/archidroid/etc/hosts
+	mv $ADDEV/ADBLOCK_USE_ADAWAY_HOSTS $ADDEV/ADBLOCK_USE_MOAB_HOSTS
+	echo "Adblock now uses Mother-Of-Ad-Blocking hosts file, consuming about 25 megabytes of memory"
+elif [ -e $ADDEV/ADBLOCK_USE_MOAB_HOSTS ]; then
+	rm -f /system/archidroid/etc/hosts
+	ln -s /system/archidroid/etc/hosts_adaway /system/archidroid/etc/hosts
+	mv $ADDEV/ADBLOCK_USE_MOAB_HOSTS $ADDEV/ADBLOCK_USE_ADAWAY_HOSTS
+	echo "Adblock now uses AdAway hosts file, consuming about 3 megabytes of memory"
+else
+	sysro
+	echo "Could not find actual hosts status!"
+	exit 1
+fi
+
+sysro
+sh /system/etc/init.d/99ArchiDroid_Init "RELOAD" "ADBLOCK"
+ndc resolver flushdefaultif >/dev/null # Flush DNS Cache
+exit 0

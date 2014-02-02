7cf4ec9bf1bed592b12d8c6f0ff3019576e4bb77
JustArchi <JustArchi@JustArchi.net>
2014-02-02 01:11:06 +0100

    Delete media2zero We don't want it anymore, as we're 4.3+ in 1.X and 2.X now

--- _archidroid/scripts/archidroid.sh
+++ _archidroid/scripts/archidroid.sh
@@ -3,19 +3,6 @@
 AD="/data/media/0/ArchiDroid"
 touch /data/ARCHIDROID_DONT_REMOVE_ME
 
-if [ ! -d /data/media/0 ]; then
-	if [ -f /data/media/0 ]; then
-		rm -f /data/media/0
-	fi
-	cd /data/media
-	FILES=`ls`
-	mkdir -p /data/media/0
-	
-	for i in $FILES; do
-		mv "$i" 0/
-	done
-fi
-
 if [ ! -d $AD ]; then
 	mkdir -p $AD
 fi

4a3d809c66bfdb307593c5c8e72ee7126fd40b64
JustArchi <JustArchi@JustArchi.net>
2014-02-02 23:42:04 +0100

    Implement proper f2fs support to admount.sh Now it'll mount f2fs filesystems as long as recovery is able to do so

--- META-INF/com/google/android/aroma/exec/admount.sh
+++ META-INF/com/google/android/aroma/exec/admount.sh
@@ -20,6 +20,7 @@ AUTO="/efs /system /cache /preload /data /storage/sdcard1" # Filesystems which s
 
 GOTBUSYBOX=false
 GOTMOUNT=false
+LOG="/tmp/archidroid_mount.log" # We can use /dev/null if not required
 
 ADMOUNTED() {
 	if [ `mount | grep -i "$1" | wc -l` -gt 0 ]; then
@@ -31,30 +32,51 @@ ADMOUNTED() {
 
 ADMOUNT() {
 	if !(ADMOUNTED "$1"); then
-		# Automatic filesystem
+		mkdir -p "$1"
+		# Stage 1, let's use automatic filesystem and block path, as recovery should have valid fstab file. This will be enough in most scenarios.
 		if $GOTBUSYBOX; then
 			busybox mount -t auto "$1" >/dev/null 2>&1
 			if (ADMOUNTED "$1"); then
-				echo "Mounted $1 through automatic busybox mount"
+				echo "Stage 1: Successfully mounted $1 through busybox mount" >> $LOG
 				return 0
 			fi
 		fi
 		if $GOTMOUNT; then
 			mount -t auto "$1" >/dev/null 2>&1
 			if (ADMOUNTED "$1"); then
-				echo "Mounted $1 through automatic mount"
+				echo "Stage 1: Successfully mounted $1 through mount" >> $LOG
 				return 0
 			fi
 		fi
-		# Still not mounted?
+		# Stage 2, mounted device isn't available in fstab and/or recovery can't mount it without such information. This is typical for f2fs, as fstab has ext4 declared. In addition to Stage 1, we'll provide block path, this should be enough.
 		if !(ADMOUNTED "$1"); then
-			# OK, fallback to predefined values then
+			if $GOTBUSYBOX; then
+				MNTPATH=`echo $1 | sed 's/\///g'`
+				eval "MNTPATH=\$$MNTPATH"
+				busybox mount -t auto "$MNTPATH" "$1" >/dev/null 2>&1
+				if (ADMOUNTED "$1"); then
+					echo "Stage 2: Successfully mounted $1 through busybox mount and $MNTPATH" >> $LOG
+					return 0
+				fi
+			fi
+			if $GOTMOUNT; then
+				MNTPATH=`echo $1 | sed 's/\///g'`
+				eval "MNTPATH=\$$MNTPATH"
+				mount -t $auto "$MNTPATH" "$1" >/dev/null 2>&1
+				if (ADMOUNTED "$1"); then
+					echo "Stage 2: Successfully mounted $1 through mount and $MNTPATH" >> $LOG
+					return 0
+				fi
+			fi
+		fi
+		# Stage 3, we failed using automatic filesystem, so we'll now use full mount command. This is our last chance.
+		if !(ADMOUNTED "$1"); then
 			if $GOTBUSYBOX; then
 				MNTPATH=`echo $1 | sed 's/\///g'`
 				eval "MNTPATH=\$$MNTPATH"
 				busybox mount -t "$fs" "$MNTPATH" "$1" >/dev/null 2>&1
 				if (ADMOUNTED "$1"); then
-					echo "Mounted $1 through predefined busybox mount using $fs and $MNTPATH"
+					echo "Stage 3: Successfully mounted $1 through busybox mount, using $fs filesystem and $MNTPATH" >> $LOG
 					return 0
 				fi
 			fi
@@ -63,42 +85,38 @@ ADMOUNT() {
 				eval "MNTPATH=\$$MNTPATH"
 				mount -t "$fs" "$MNTPATH" "$1" >/dev/null 2>&1
 				if (ADMOUNTED "$1"); then
-					echo "Mounted $1 through predefined mount using $fs and $MNTPATH"
+					echo "Stage 3: Successfully mounted $1 through mount, using $fs filesystem and $MNTPATH" >> $LOG
 					return 0
 				fi
 			fi
 		fi
-		# Ok, I give up
+		# Stage 4, we're out of ideas
 		if !(ADMOUNTED "$1"); then
-			echo "Could not mount $1"
+			echo "Stage 4: ERROR! Could not mount $1" >> $LOG
 			return 1
 		fi
 	else
-		echo "$1 is already mounted"
+		echo "$1 is already mounted" >> $LOG
 	fi
 	return 0
 }
 
 if [ ! -z `which busybox` ]; then
-	echo "Got busybox!"
 	GOTBUSYBOX=true
 fi
 if [ ! -z `which mount` ]; then
-	echo "Got mount!"
 	GOTMOUNT=true
 fi
 if (! $GOTBUSYBOX && ! $GOTMOUNT); then
 	# This should never happen, but safety check is always good
-	echo "FATAL ERROR, NO BUSYBOX NEITHER MOUNT"
+	echo "FATAL ERROR, NO BUSYBOX NEITHER MOUNT" >> $LOG
 	exit 1
 fi
 
 if [ -z "$1" ]; then
 	# No argument given, mount AUTO
 	for mnt in $AUTO; do
-		if [ ! -z "$mnt" ]; then
-			ADMOUNT "$mnt"
-		fi
+		ADMOUNT "$mnt"
 	done
 else
 	ADMOUNT "$1"

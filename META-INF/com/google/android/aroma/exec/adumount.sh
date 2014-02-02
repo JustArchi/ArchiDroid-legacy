f847f5445c7e303be06b97076abcd127fdcc56d2
JustArchi <JustArchi@JustArchi.net>
2014-02-02 23:53:04 +0100

    Adapt these misc fixes also to adumount.sh

--- META-INF/com/google/android/aroma/exec/adumount.sh
+++ META-INF/com/google/android/aroma/exec/adumount.sh
@@ -9,6 +9,7 @@ AUTO="/efs /system /cache /preload /data /storage/sdcard1" # Filesystems which s
 
 GOTBUSYBOX=false
 GOTMOUNT=false
+LOG="/tmp/archidroid_mount.log" # We can use /dev/null if not required
 
 ADMOUNTED() {
 	if [ `mount | grep -i "$1" | wc -l` -gt 0 ]; then
@@ -20,52 +21,47 @@ ADMOUNTED() {
 
 ADUMOUNT() {
 	if (ADMOUNTED "$1"); then
-		# Automatic filesystem
 		if $GOTBUSYBOX; then
-			busybox umount "$1" >/dev/null 2>&1
+			busybox umount -f "$1" >/dev/null 2>&1
 			if !(ADMOUNTED "$1"); then
-				echo "Unmounted $1 through busybox umount"
+				echo "Successfully unmounted $1 through busybox umount" >> $LOG
 				return 0
 			fi
 		fi
 		if $GOTMOUNT; then
-			umount "$1" >/dev/null 2>&1
+			umount -f "$1" >/dev/null 2>&1
 			if !(ADMOUNTED "$1"); then
-				echo "Unmounted $1 through umount"
+				echo "Successfully unmounted $1 through umount" >> $LOG
 				return 0
 			fi
 		fi
 		# Ok, I give up
 		if (ADMOUNTED "$1"); then
-			echo "Could not unmount $1"
+			echo "ERROR: Could not unmount $1" >> $LOG
 			return 1
 		fi
 	else
-		echo "$1 is already unmounted"
+		echo "$1 is already unmounted" >> $LOG
 	fi
 	return 0
 }
 
 if [ ! -z `which busybox` ]; then
-	echo "Got busybox!"
 	GOTBUSYBOX=true
 fi
 if [ ! -z `which mount` ]; then
-	echo "Got mount!"
 	GOTMOUNT=true
 fi
 if (! $GOTBUSYBOX && ! $GOTMOUNT); then
 	# This should never happen, but safety check is always good
-	echo "FATAL ERROR, NO BUSYBOX NEITHER MOUNT"
+	echo "FATAL ERROR, NO BUSYBOX NEITHER MOUNT" >> $LOG
 	exit 1
 fi
 
 if [ -z "$1" ]; then
 	# No argument given, umount AUTO
 	for mnt in $AUTO; do
-		if [ ! -z "$mnt" ]; then
-			ADUMOUNT "$mnt"
-		fi
+		ADUMOUNT "$mnt"
 	done
 else
 	ADUMOUNT "$1"

cb5a967f1c86f9ec3e3317652a377341bf94c769
JustArchi <JustArchi@JustArchi.net>
2014-02-15 10:05:15 +0100

    Update langs

--- __dont_include/addtolangs.sh
+++ __dont_include/addtolangs.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
-if [ -z "$1" ] || [ -z "$2" ]; then
+if [ $# -lt 2 ]; then
 	exit 1
 fi
 
-AFTERWHAT="$1"
-ADDWHAT="$2"
 cd ../META-INF/com/google/android/aroma/langs
-for FILE in `ls`; do
-	LINES=`cat $FILE | wc -l`
-	WHERE=`grep -n "$AFTERWHAT" $FILE | cut -f1 -d:`
-	cat $FILE | head -${WHERE} > temp.txt
-	echo "$ADDWHAT" >> temp.txt
-	LINES=`expr $LINES - $WHERE`
-	cat $FILE | tail -${LINES} >> temp.txt
-	cp temp.txt $FILE
-	rm -f temp.txt
-done
+if [ "$1" == "ADD" ]; then
+	for FILE in `ls`; do
+		LINES=`cat $FILE | wc -l`
+		WHERE=`grep -n "$2" $FILE | cut -f1 -d:`
+		cat $FILE | head -${WHERE} > temp.txt
+		echo "$3" >> temp.txt
+		LINES=`expr $LINES - $WHERE`
+		cat $FILE | tail -${LINES} >> temp.txt
+		cp temp.txt $FILE
+		rm -f temp.txt
+	done
+elif [ "$1" == "DEL" ]; then
+	for FILE in `ls`; do
+		LINES=`cat $FILE | wc -l`
+		WHERE=`grep -n "$2" $FILE | cut -f1 -d:`
+		WHERE=`expr $WHERE - 1`
+		cat $FILE | head -${WHERE} > temp.txt
+		WHERE=`expr $WHERE + 1`
+		LINES=`expr $LINES - $WHERE`
+		cat $FILE | tail -${LINES} >> temp.txt
+		cp temp.txt $FILE
+		rm -f temp.txt
+	done
+fi
 exit 0

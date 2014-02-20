30657e2ffcdef77536f794cfa2150dd79817a048
JustArchi <JustArchi@JustArchi.net>
2014-02-20 22:04:55 +0100

    Fix dns daemon duplicates

--- _archidroid/auto/system/archidroid/scripts/DEPRECATED/ArchiDroid_Adblock_EnableDisableLocalDNSes.sh
+++ _archidroid/auto/system/archidroid/scripts/DEPRECATED/ArchiDroid_Adblock_EnableDisableLocalDNSes.sh
@@ -10,16 +10,13 @@ ADDEV="/system/archidroid/dev"
 sysrw
 
 if [ -e $ADDEV/ADBLOCK_LOCAL_DNSES_ENABLED ]; then
-	rm -f $ADDEV/ADBLOCK_LOCAL_DNSES_DAEMON_ENABLED
 	rm -f $ADDEV/ADBLOCK_FORCE_LOCAL_DNSES
-	touch $ADDEV/ADBLOCK_LOCAL_DNSES_DAEMON_DISABLED
 	touch $ADDEV/ADBLOCK_DONT_FORCE_LOCAL_DNSES
 	mv $ADDEV/ADBLOCK_LOCAL_DNSES_ENABLED $ADDEV/ADBLOCK_LOCAL_DNSES_DISABLED
 	echo "Local DNSes are now ignored by Adblock"
 elif [ -e $ADDEV/ADBLOCK_LOCAL_DNSES_DISABLED ]; then
 	mv $ADDEV/ADBLOCK_LOCAL_DNSES_DISABLED $ADDEV/ADBLOCK_LOCAL_DNSES_ENABLED
 	echo "Local DNSes are now being used by Adblock"
-	echo "Keep in mind that they're not automatically updated, if you want them to be, don't forget to enable LocalDNSesDaemon as well"
 else
 	sysro
 	echo "Could not find actual adblock DNSes status!"

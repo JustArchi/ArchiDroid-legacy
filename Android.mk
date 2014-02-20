264354d52497563db8d50a802157dc3fa1fce978
JustArchi <JustArchi@JustArchi.net>
2014-02-20 15:34:48 +0100

    Final touch

--- Android.mk
+++ Android.mk
@@ -1,21 +1,22 @@
-LOCAL_PATH:= $(call my-dir)
+LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := tests
 
-LOCAL_STATIC_JAVA_LIBRARIES := android-support-v13 RootTools
+LOCAL_STATIC_JAVA_LIBRARIES := RootTools android-support-v13
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_SDK_VERSION := current
 
 LOCAL_PACKAGE_NAME := ArchiDroid
-LOCAL_CERTIFICATE := platform
 
 include $(BUILD_PACKAGE)
 ##################################################
 include $(CLEAR_VARS)
 
-LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := RootTools:libs/RootTools-3.4.jar
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := RootTools:libs/RootTools.jar
+
+include $(BUILD_MULTI_PREBUILT)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))

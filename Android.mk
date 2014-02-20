e83396b725ef782582fb6387a55b56edf3cda786
JustArchi <JustArchi@JustArchi.net>
2014-02-20 15:27:28 +0100

    Another try

--- Android.mk
+++ Android.mk
@@ -3,15 +3,19 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := tests
 
-LOCAL_STATIC_JAVA_LIBRARIES := \
-android-support-v13 \
-RootTools-3.4.jar
+LOCAL_STATIC_JAVA_LIBRARIES := android-support-v13 RootTools
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
+
+LOCAL_SDK_VERSION := current
 
 LOCAL_PACKAGE_NAME := ArchiDroid
 LOCAL_CERTIFICATE := platform
 
 include $(BUILD_PACKAGE)
+##################################################
+include $(CLEAR_VARS)
+
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := RootTools:libs/RootTools-3.4.jar
 
 include $(call all-makefiles-under,$(LOCAL_PATH))

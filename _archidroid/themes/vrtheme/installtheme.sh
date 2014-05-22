aaf3c37b60cb932de0c0bd9cd6dffa1536824820
JustArchi <JustArchi@JustArchi.net>
2014-05-22 19:21:12 +0200

    Initial try on odexed roms

--- _archidroid/themes/vrtheme/installtheme.sh
+++ _archidroid/themes/vrtheme/installtheme.sh
@@ -22,39 +22,71 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# NOTICE #1
-# Yes, you're right, this was initially written to allow easy parallel jobs in background.
-# By simply adding & to the end of our WORK command we would run all these commands in parallel
-# which would drastically reduce time needed for doing all of these tasks
-# HOWEVER, unfortunately, AROMA or CWM recovery doesn't allow background tasks
-# and immediately crashes when the first one is launched
-# Therefore, we can't use background task at least now, maybe in future...
-
 VRDIR="/cache/vrtheme"
 VALIDTARGETS="app framework priv-app"
+TOOLS="zip zipalign dexopt-wrapper"
+LOG="$VRDIR/log.txt" # Can be /dev/null
 
-WORK() {
+WORK_DEODEX() {
 	# $1 - Target dir such as /system/app or /system/framework
 	# $2 - Apk name such as SystemUI.apk or framework-res.apk
 	if [ -f "$1/$2" ]; then
+		echo "MODDING: $1/$2"
 		cd "$VRDIR/$1/$2" &&
-		"$VRDIR/zip" -r "$1/$2" ./* &&
-		"$VRDIR/zipalign" -f 4 "$1/$2" "$1/$2.aligned" &&
+		"$VRDIR/zip" -r "$1/$2" ./* >/dev/null &&
+		"$VRDIR/zipalign" -f 4 "$1/$2" "$1/$2.aligned" >/dev/null &&
 		mv -f "$1/$2.aligned" "$1/$2" &&
-		chmod 644 "$1/$2"
+		chmod 644 "$1/$2" &&
+		echo "OK: $1/$2"
+	else
+		echo "WARNING: $1/$2 was found for modding but there's no such file in the ROM!"
+		return 1
 	fi
 }
 
-for TARGET in $VALIDTARGETS; do 
-	if [ -d "$VRDIR/system/$TARGET" ]; then
-		find "$VRDIR/system/$TARGET" -mindepth 1 -maxdepth 1 -type d | while read APK; do
-			#WORK "/system/$TARGET" "$(basename "$APK")" & # NOTICE #1
-			WORK "/system/$TARGET" "$(basename "$APK")"
-		done
+WORK_ODEX() {
+	local ODEXFILE="$(echo "$1/$2" | rev | cut -d'.' -f2- | rev).odex"
+	WORK_DEODEX "$1" "$2" || return
+	if [ -f "$VRDIR/$1/$2/classes.dex" ]; then
+		echo "ODEX: Found classes.dex in new $2"
+		rm -f "$ODEXFILE"
+		"$VRDIR/dexopt-wrapper" "$1/$2" "$ODEXFILE" >/dev/null || echo "ODEX: Could not odex $1/$2"
 	fi
+}
+
+#exec 1>"$LOG"
+#exec 2>&1
+
+for TOOL in $TOOLS; do
+	chmod 755 "$VRDIR/$TOOL"
 done
 
-#wait # NOTICE #1
+if [ -f "/system/framework/framework.odex" ]; then
+	echo "INFO: Using ODEX mode"
+	# Define BOOTCLASSPATH as all available JARs
+	BOOTCLASSPATH=""
+	for f in /system/framework/*.jar; do
+		BOOTCLASSPATH="$BOOTCLASSPATH$f:"
+	done
+	export BOOTCLASSPATH="$BOOTCLASSPATH"
+
+	for TARGET in $VALIDTARGETS; do 
+		if [ -d "$VRDIR/system/$TARGET" ]; then
+			find "$VRDIR/system/$TARGET" -mindepth 1 -maxdepth 1 -type d | while read APK; do
+				WORK_ODEX "/system/$TARGET" "$(basename "$APK")"
+			done
+		fi
+	done
+else
+	echo "INFO: Using DEODEX mode"
+	for TARGET in $VALIDTARGETS; do 
+		if [ -d "$VRDIR/system/$TARGET" ]; then
+			find "$VRDIR/system/$TARGET" -mindepth 1 -maxdepth 1 -type d | while read APK; do
+				WORK_DEODEX "/system/$TARGET" "$(basename "$APK")"
+			done
+		fi
+	done
+fi
 
 rm -rf "$VRDIR"
 exit 0

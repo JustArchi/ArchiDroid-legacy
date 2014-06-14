df505f720a353b7e46c5ed46b9824eda94d30ff1
JustArchi <JustArchi@JustArchi.net>
2014-06-14 22:32:56 +0200

    Make barebones preset even more bare

--- _archidroid/exec/archidroid_backend.sh
+++ _archidroid/exec/archidroid_backend.sh
@@ -21,27 +21,40 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# ArchiDroid Backend Fallback
-if [ ! -f "/system/bin/debuggerd.real" ]; then
-	mv "/system/bin/debuggerd" "/system/bin/debuggerd.real"
-fi
-mv "/system/bin/addebuggerd" "/system/bin/debuggerd"
+BAREBONES=0
 
-# ArchiDroid Dnsmasq Fallback
-if [ ! -f "/system/bin/dnsmasq.real" ]; then
-	mv "/system/bin/dnsmasq" "/system/bin/dnsmasq.real"
-fi
-mv "/system/bin/addnsmasq" "/system/bin/dnsmasq"
-
-# ArchiDroid Adblock Hosts
-if [ ! -L "/system/archidroid/etc/hosts" ]; then
-	ln -s "/system/archidroid/etc/hosts_adaway" "/system/archidroid/etc/hosts"
-fi
+for ARG in "$@"; do
+	case "$ARG" in
+		barebones) BAREBONES=1 ;;
+	esac
+done
 
 # SuperSU
 mkdir -p "/system/bin/.ext"
 cp "/system/xbin/su" "/system/xbin/daemonsu"
 cp "/system/xbin/su" "/system/bin/.ext/.su"
+
+# Apply additional ArchiDroid things if we're not on barebones
+if [ "$BAREBONES" -eq 0 ]; then
+	# ArchiDroid Backend Fallback
+	if [ ! -f "/system/bin/debuggerd.real" ]; then
+		mv "/system/bin/debuggerd" "/system/bin/debuggerd.real"
+	fi
+	mv "/system/bin/addebuggerd" "/system/bin/debuggerd"
+
+	# ArchiDroid Dnsmasq Fallback
+	if [ ! -f "/system/bin/dnsmasq.real" ]; then
+		mv "/system/bin/dnsmasq" "/system/bin/dnsmasq.real"
+	fi
+	mv "/system/bin/addnsmasq" "/system/bin/dnsmasq"
+
+	# ArchiDroid Adblock Hosts
+	if [ ! -L "/system/archidroid/etc/hosts" ]; then
+		ln -s "/system/archidroid/etc/hosts_adaway" "/system/archidroid/etc/hosts"
+	fi
+else
+	touch "/system/archidroid/dev/PRESET_BAREBONES"
+fi
 
 sync
 exit 0

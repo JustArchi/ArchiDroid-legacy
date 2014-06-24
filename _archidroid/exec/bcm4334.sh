6369011d20fca2c9a3ea4bf70e736583ccf6d1e0
JustArchi <JustArchi@JustArchi.net>
2014-06-24 14:50:33 +0200

    EXPERIMENTAL: bcm4334 profiling

---
+++ _archidroid/exec/bcm4334.sh
@@ -0,0 +1,34 @@
+#!/sbin/sh
+
+#     _             _     _ ____            _     _
+#    / \   _ __ ___| |__ (_)  _ \ _ __ ___ (_) __| |
+#   / _ \ | '__/ __| '_ \| | | | | '__/ _ \| |/ _` |
+#  / ___ \| | | (__| | | | | |_| | | | (_) | | (_| |
+# /_/   \_\_|  \___|_| |_|_|____/|_|  \___/|_|\__,_|
+#
+# Copyright 2014 Łukasz "JustArchi" Domeradzki
+# Contact: JustArchi@JustArchi.net
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -e
+
+if [ "$1" = "stock" ]; then
+	exit 0
+elif [ -z "$1" -o ! -f "/system/bin/bcm4334.hcd" -o ! -f "/system/bin/bcm4334_$1.hcd" ]; then
+	exit 1
+fi
+
+cp -p "/system/bin/bcm4334_$1.hcd" "/system/bin/bcm4334.hcd"
+sync
+exit 0

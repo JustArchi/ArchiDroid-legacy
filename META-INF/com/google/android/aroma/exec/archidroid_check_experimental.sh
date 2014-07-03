71f6d14f15c48c5a9500174a7239ea7667de47e7
JustArchi <JustArchi@JustArchi.net>
2014-07-03 02:59:21 +0200

    misc

--- META-INF/com/google/android/aroma/exec/archidroid_check_experimental.sh
+++ META-INF/com/google/android/aroma/exec/archidroid_check_experimental.sh
@@ -21,4 +21,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-exit "$(echo "$@" | grep -q "XXth"; echo $?)"
+exit "$(echo "$@" | grep -qi "XX"; echo $?)"

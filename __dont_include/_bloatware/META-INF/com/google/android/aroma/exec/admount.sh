a35e4a4baf05d8aa48aab56a7bc87643138dff12
JustArchi <JustArchi@JustArchi.net>
2014-07-30 23:13:55 +0200

    Update bloatware flasher

--- __dont_include/_bloatware/META-INF/com/google/android/aroma/exec/admount.sh
+++ __dont_include/_bloatware/META-INF/com/google/android/aroma/exec/admount.sh
@@ -1,109 +1,121 @@
 #!/sbin/sh
 
-# JustArchi@JustArchi.net
+#     _             _     _ ____            _     _
+#    / \   _ __ ___| |__ (_)  _ \ _ __ ___ (_) __| |
+#   / _ \ | '__/ __| '_ \| | | | | '__/ _ \| |/ _` |
+#  / ___ \| | | (__| | | | | |_| | | | (_) | | (_| |
+# /_/   \_\_|  \___|_| |_|_|____/|_|  \___/|_|\__,_|
+#
+# Copyright 2014 Łukasz "JustArchi" Domeradzki
+# Contact: JustArchi@JustArchi.net
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 # Used by ArchiDroid for providing universal device-based paths
 # Usage: admount.sh *path*, f.e. admount.sh /storage/sdcard1
 
 # These are absolute paths without slashes, for example /storage/sdcard1 is storagesdcard1, because you can't use / in variables
 fs="ext4" # Filesystem
-efs="/dev/block/mmcblk0p3" # EFS, if available
-boot="/dev/block/mmcblk0p5" # ROM's kernel image
-recovery="/dev/block/mmcblk0p6" # Recovery image
-radio="/dev/block/mmcblk0p7" # Modem image
-cache="/dev/block/mmcblk0p8" # Cache partition
-system="/dev/block/mmcblk0p9" # System partition
-preload="/dev/block/mmcblk0p10" # Preload partition (also SELinux)
-data="/dev/block/mmcblk0p12" # Data and internal memory
-storagesdcard1="/dev/block/mmcblk1p1" # External memory, if available
-AUTO="/efs /system /cache /preload /data /storage/sdcard1" # Filesystems which should be mounted automatically when no argument is given, typically every partition excluding images
+efs="/dev/block/mmcblk0p3" # EFS, if available, used by eval in stages 2+, SC2034
+cache="/dev/block/mmcblk0p8" # Cache partition, used by eval in stages 2+, SC2034
+system="/dev/block/mmcblk0p9" # System partition, used by eval in stages 2+, SC2034
+preload="/dev/block/mmcblk0p10" # Preload partition (also SELinux), used by eval in stages 2+, SC2034
+data="/dev/block/mmcblk0p12" # Data and internal memory, used by eval in stages 2+, SC2034
+storagesdcard1="/dev/block/mmcblk1p1" # External memory, if available, used by eval in stages 2+, SC2034
+AUTO="/efs /cache /system /preload /data /storage/sdcard1" # Filesystems which should be unmounted automatically when no argument is given, typically every partition excluding images
 
-GOTBUSYBOX=false
-GOTMOUNT=false
-LOG="/tmp/archidroid_mount.log" # We can use /dev/null if not required
+GOTBUSYBOX="false"
+GOTMOUNT="false"
+LOG="/dev/null" # We can use /dev/null if not required
 
 ADMOUNTED() {
-	if [ `mount | grep -i "$1" | wc -l` -gt 0 ]; then
-		return 0
-	else
-		return 1
-	fi
+	return "$(mount | grep -qi "$1"; echo $?)"
 }
 
 ADMOUNT() {
-	if !(ADMOUNTED "$1"); then
+	if ! ADMOUNTED "$1"; then
 		mkdir -p "$1"
 		# Stage 1, let's use automatic filesystem and block path, as recovery should have valid fstab file. This will be enough in most scenarios.
-		if $GOTBUSYBOX; then
+		if "$GOTBUSYBOX"; then
 			busybox mount -t auto "$1" >/dev/null 2>&1
-			if (ADMOUNTED "$1"); then
-				echo "Stage 1: Successfully mounted $1 through busybox mount" >> $LOG
+			if ADMOUNTED "$1"; then
+				echo "Stage 1: Successfully mounted $1 through busybox mount" >> "$LOG"
 				return 0
 			fi
 		fi
-		if $GOTMOUNT; then
+		if "$GOTMOUNT"; then
 			mount -t auto "$1" >/dev/null 2>&1
-			if (ADMOUNTED "$1"); then
-				echo "Stage 1: Successfully mounted $1 through mount" >> $LOG
+			if ADMOUNTED "$1"; then
+				echo "Stage 1: Successfully mounted $1 through mount" >> "$LOG"
 				return 0
 			fi
 		fi
 		# Stage 2, mounted device isn't available in fstab and/or recovery can't mount it without such information. This is typical for f2fs, as fstab has ext4 declared. In addition to Stage 1, we'll provide block path, this should be enough.
-		if $GOTBUSYBOX; then
-			MNTPATH=`echo $1 | sed 's/\///g'`
-			eval "MNTPATH=\$$MNTPATH"
+		if "$GOTBUSYBOX"; then
+			MNTPATH="$(echo "$1" | sed 's/\///g')"
+			eval "MNTPATH=\$$MNTPATH" # EVAL IS EVIL
 			busybox mount -t auto "$MNTPATH" "$1" >/dev/null 2>&1
-			if (ADMOUNTED "$1"); then
-				echo "Stage 2: Successfully mounted $1 through busybox mount and $MNTPATH" >> $LOG
+			if ADMOUNTED "$1"; then
+				echo "Stage 2: Successfully mounted $1 through busybox mount and $MNTPATH" >> "$LOG"
 				return 0
 			fi
 		fi
-		if $GOTMOUNT; then
-			MNTPATH=`echo $1 | sed 's/\///g'`
-			eval "MNTPATH=\$$MNTPATH"
-			mount -t $auto "$MNTPATH" "$1" >/dev/null 2>&1
-			if (ADMOUNTED "$1"); then
-				echo "Stage 2: Successfully mounted $1 through mount and $MNTPATH" >> $LOG
+		if "$GOTMOUNT"; then
+			MNTPATH="$(echo "$1" | sed 's/\///g')"
+			eval "MNTPATH=\$$MNTPATH" # EVAL IS EVIL
+			mount -t auto "$MNTPATH" "$1" >/dev/null 2>&1
+			if ADMOUNTED "$1"; then
+				echo "Stage 2: Successfully mounted $1 through mount and $MNTPATH" >> "$LOG"
 				return 0
 			fi
 		fi
 		# Stage 3, we failed using automatic filesystem, so we'll now use full mount command. This is our last chance.
-		if $GOTBUSYBOX; then
-			MNTPATH=`echo $1 | sed 's/\///g'`
-			eval "MNTPATH=\$$MNTPATH"
+		if "$GOTBUSYBOX"; then
+			MNTPATH="$(echo "$1" | sed 's/\///g')"
+			eval "MNTPATH=\$$MNTPATH" # EVAL IS EVIL
 			busybox mount -t "$fs" "$MNTPATH" "$1" >/dev/null 2>&1
-			if (ADMOUNTED "$1"); then
-				echo "Stage 3: Successfully mounted $1 through busybox mount, using $fs filesystem and $MNTPATH" >> $LOG
+			if ADMOUNTED "$1"; then
+				echo "Stage 3: Successfully mounted $1 through busybox mount, using $fs filesystem and $MNTPATH" >> "$LOG"
 				return 0
 			fi
 		fi
-		if $GOTMOUNT; then
-			MNTPATH=`echo $1 | sed 's/\///g'`
-			eval "MNTPATH=\$$MNTPATH"
+		if "$GOTMOUNT"; then
+			MNTPATH="$(echo "$1" | sed 's/\///g')"
+			eval "MNTPATH=\$$MNTPATH" # EVAL IS EVIL
 			mount -t "$fs" "$MNTPATH" "$1" >/dev/null 2>&1
-			if (ADMOUNTED "$1"); then
-				echo "Stage 3: Successfully mounted $1 through mount, using $fs filesystem and $MNTPATH" >> $LOG
+			if ADMOUNTED "$1"; then
+				echo "Stage 3: Successfully mounted $1 through mount, using $fs filesystem and $MNTPATH" >> "$LOG"
 				return 0
 			fi
 		fi
 		# Stage 4, we're out of ideas
-		echo "Stage 4: ERROR! Could not mount $1" >> $LOG
+		echo "Stage 4: ERROR! Could not mount $1" >> "$LOG"
 		return 1
 	else
-		echo "$1 is already mounted" >> $LOG
+		echo "$1 is already mounted" >> "$LOG"
 	fi
 	return 0
 }
 
-if [ ! -z `which busybox` ]; then
-	GOTBUSYBOX=true
+if [ ! -z "$(which busybox)" ]; then
+	GOTBUSYBOX="true"
 fi
-if [ ! -z `which mount` ]; then
-	GOTMOUNT=true
+if [ ! -z "$(which mount)" ]; then
+	GOTMOUNT="true"
 fi
-if (! $GOTBUSYBOX && ! $GOTMOUNT); then
+if ! "$GOTBUSYBOX" && ! "$GOTMOUNT"; then
 	# This should never happen, but safety check is always good
-	echo "FATAL ERROR, NO BUSYBOX NEITHER MOUNT" >> $LOG
+	echo "FATAL ERROR, NO BUSYBOX NEITHER MOUNT" >> "$LOG"
 	exit 1
 fi
 
@@ -116,4 +128,5 @@ else
 	ADMOUNT "$1"
 fi
 
+sync
 exit 0
--- __dont_include/_bloatware/META-INF/com/google/android/aroma/exec/archidroid_detect.sh
+++ __dont_include/_bloatware/META-INF/com/google/android/aroma/exec/archidroid_detect.sh
@@ -1,20 +1,40 @@
 #!/sbin/sh
 
-# exit 0 -> Internal error, no build.prop detected. Full wipe?
+#     _             _     _ ____            _     _
+#    / \   _ __ ___| |__ (_)  _ \ _ __ ___ (_) __| |
+#   / _ \ | '__/ __| '_ \| | | | | '__/ _ \| |/ _` |
+#  / ___ \| | | (__| | | | | |_| | | | (_) | | (_| |
+# /_/   \_\_|  \___|_| |_|_|____/|_|  \___/|_|\__,_|
+#
+# Copyright 2014 Łukasz "JustArchi" Domeradzki
+# Contact: JustArchi@JustArchi.net
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# exit 0 -> No build.prop detected, we can't check ArchiDroid existance. Full wipe?
 # exit 1 -> All fine, we're NOT running ArchiDroid
 # exit 2 -> All fine, we're running ArchiDroid
 
-if [ -e /system/build.prop ]; then
-	# Good
-	if [ `cat /system/build.prop | grep -i "ArchiDroid" | wc -l` -gt 0 ]; then
-		# Yay we're running ArchiDroid"
+if [ -f "/system/build.prop" ]; then
+	# We're not after full wipe
+	if [ "$(grep -qi "ArchiDroid" "/system/build.prop"; echo $?)" -eq 0 ]; then
+		# We're running ArchiDroid"
 		exit 2
 	else
 		# We're not running ArchiDroid
 		exit 1
 	fi
-else
-	# Report internal error
-	exit 0
 fi
+
+sync
 exit 0

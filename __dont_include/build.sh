37db96a3a7e5ffb6b8290769ef3e99b959ddfcf5
JustArchi <JustArchi@JustArchi.net>
2014-08-13 13:32:59 +0200

    Bump to 2.5.4

--- __dont_include/build.sh
+++ __dont_include/build.sh
@@ -22,7 +22,7 @@
 # limitations under the License.
 
 set -e
-VERSION=2.5.3
+VERSION=2.5.4
 
 # HOW TO PORT ARCHIDROID TO OTHER DEVICE
 # 1. Make sure you have a good base, this can be a stock ROM or AOSP ROM. Put it in .zip format in the root of ArchiDroid.

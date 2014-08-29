f974df6c91d37277012d7e36dd1b0bcb4c3ed91f
JustArchi <JustArchi@JustArchi.net>
2014-08-29 10:02:01 +0200

    Updated sources (Linaro 4.9.2)

--- recovery/etc/install-cm-recovery.sh
+++ recovery/etc/install-cm-recovery.sh
@@ -1,11 +1,11 @@
 #!/system/bin/sh
 if [ -f /system/etc/recovery-transform.sh ]; then
-  exec sh /system/etc/recovery-transform.sh 7989248 b60148a58d794ef1796a15cf0a44e7631c1c93dd 5087232 9c18c371a2eaa4a6ee9dfb6c725408760e2e7643
+  exec sh /system/etc/recovery-transform.sh 8013824 db6410345ce8d5844eabacb8320b1b7a2e38de90 5101568 12d2453311b9f1219bceaa8e93c6f655d5b6a0eb
 fi
 
-if ! applypatch -c EMMC:/dev/block/mmcblk0p6:7989248:b60148a58d794ef1796a15cf0a44e7631c1c93dd; then
+if ! applypatch -c EMMC:/dev/block/mmcblk0p6:8013824:db6410345ce8d5844eabacb8320b1b7a2e38de90; then
   log -t recovery "Installing new recovery image"
-  applypatch -b /system/etc/recovery-resource.dat EMMC:/dev/block/mmcblk0p5:5087232:9c18c371a2eaa4a6ee9dfb6c725408760e2e7643 EMMC:/dev/block/mmcblk0p6 b60148a58d794ef1796a15cf0a44e7631c1c93dd 7989248 9c18c371a2eaa4a6ee9dfb6c725408760e2e7643:/system/recovery-from-boot.p
+  applypatch -b /system/etc/recovery-resource.dat EMMC:/dev/block/mmcblk0p5:5101568:12d2453311b9f1219bceaa8e93c6f655d5b6a0eb EMMC:/dev/block/mmcblk0p6 db6410345ce8d5844eabacb8320b1b7a2e38de90 8013824 12d2453311b9f1219bceaa8e93c6f655d5b6a0eb:/system/recovery-from-boot.p
 else
   log -t recovery "Recovery image already installed"
 fi

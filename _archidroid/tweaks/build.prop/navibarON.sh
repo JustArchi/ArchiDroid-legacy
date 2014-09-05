f71868d379f9585ebf064ddb85d2406165d82d50
JustArchi <JustArchi@JustArchi.net>
2014-09-05 22:09:11 +0200

    Disable corresponding hardware keys when navigation bar is enabled

--- _archidroid/tweaks/build.prop/navibarON.sh
+++ _archidroid/tweaks/build.prop/navibarON.sh
@@ -1,5 +1,35 @@
 #!/sbin/sh
-sed -i 's/#qemu.hw.mainkeys=0/qemu.hw.mainkeys=0/g' /system/build.prop
+
+#     _             _     _ ____            _     _
+#    / \   _ __ ___| |__ (_)  _ \ _ __ ___ (_) __| |
+#   / _ \ | '__/ __| '_ \| | | | | '__/ _ \| |/ _` |
+#  / ___ \| | | (__| | | | | |_| | | | (_) | | (_| |
+# /_/   \_\_|  \___|_| |_|_|____/|_|  \___/|_|\__,_|
+#
+# Copyright 2014 Łukasz "JustArchi" Domeradzki
+# Contact: JustArchi@JustArchi.net
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Define keys we want to disable
+KEYS="139 158 172"
+
+sed -i 's/#qemu.hw.mainkeys=0/qemu.hw.mainkeys=0/g' "/system/build.prop"
+find "/system/usr/keylayout" -type f -name "*.kl" | while read line; do
+	for KEY in $KEYS; do
+		sed -i "s/key $KEY/#key $KEY/g" "$line"
+	done
+done
 
 sync
 exit 0

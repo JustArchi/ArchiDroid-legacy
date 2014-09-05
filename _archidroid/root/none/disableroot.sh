adc44cb41347be33c02f46866085b9ff4e10c302
JustArchi <JustArchi@JustArchi.net>
2014-09-05 21:19:28 +0200

    Implement root selection

---
+++ _archidroid/root/none/disableroot.sh
@@ -0,0 +1,27 @@
+#!/sbin/sh
+
+#     _             _     _ ____            _     _
+#    / \   _ __ ___| |__ (_)  _ \ _ __ ___ (_) __| |
+#   / _ \ | '__/ __| '_ \| | | | | '__/ _ \| |/ _` |
+#  / ___ \| | | (__| | | | | |_| | | | (_) | | (_| |
+# /_/   \_\_|  \___|_| |_|_|____/|_|  \___/|_|\__,_|
+#
+# Copyright 2014 Łukasz "JustArchi" Domeradzki
+# Contact: JustArchi@JustArchi.net
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+rm -f "/system/xbin/su" "/system/app/Superuser.apk" "/system/app/Superuser.odex" "/system/priv-app/Superuser.apk" "/system/priv-app/Superuser.odex"
+sed -i "s/persist.sys.root_access=1/persist.sys.root_access=0/g" "/system/build.prop"
+sync
+exit 0
